f26ef94cae57dbfccaf7c3f29c6b2389b0ef94c0
inna1989F <fateevainnessa@gmail.com>
2026-03-06 13:12:09 +0100

    Fixed size, arrays

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -8,17 +8,27 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private V [] values;
     private int size;
 
-    @SuppressWarnings("uncheked")
+    @SuppressWarnings("unchecked")
     public StorageImpl() {
         keys = (K[]) new Object[MAX_SIZE];
         values = (V[]) new Object[MAX_SIZE];
+        size = 0;
+    }
+
+    private boolean keysEqual(K k1, K k2) {
+        if (k1 == null && k2 == null) {
+            return true;
+        }
+        if (k1 != null) {
+            return k1.equals(k2);
+        }
+        return false;
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if ((keys[i] == null && key == null)
-                    || (keys[i] != null && keys[i].equals(key))) {
+            if (keysEqual(keys[i], key)) {
                 values[i] = value;
                 return;
             }
@@ -34,9 +44,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if ((keys[i] == null && key == null)
-                    || (keys[i] != null && keys[i].equals(key))) {
-                return values[i];
+            if (keysEqual(keys[i], key)) {
+                return values [i];
             }
         }
         return null;
